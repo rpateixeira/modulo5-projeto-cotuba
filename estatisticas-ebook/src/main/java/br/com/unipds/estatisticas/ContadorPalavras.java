914723ffb1b2d3313649de43327b443a30adc1ba
rpteixeirasys <rpteixeira@stefanini.com>
2026-09-07 12:17:50 -0300

    ajustando para favorecer a composicao

--- estatisticas-ebook/src/main/java/br/com/unipds/estatisticas/ContadorPalavras.java
+++ estatisticas-ebook/src/main/java/br/com/unipds/estatisticas/ContadorPalavras.java
@@ -1,10 +1,17 @@
 package br.com.unipds.estatisticas;
 
+import java.util.Map;
 import java.util.TreeMap;
 
-class ContadorPalavras extends TreeMap<String,Integer> {
+class ContadorPalavras {
+
+    private final TreeMap<String, Integer> mapa = new TreeMap<>();
 
     public void adicionarPalavras(String palavra){
-        super.merge(palavra, 1, Integer::sum);
+        mapa.merge(palavra, 1, Integer::sum);
+    }
+
+    public Iterable<? extends Map.Entry<String, Integer>> entrySet() {
+        return mapa.entrySet();
     }
 }
